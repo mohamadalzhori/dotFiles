de085c67abcaaf0c4a1ab18ac1249d81d06bacc7
Mohamad Alzhori <99711702+mohamadalzhori@users.noreply.github.com>
2023-08-19 23:34:26 +0300

    .local/bin/scripts/

--- executor.sh
+++ executor.sh
@@ -3,8 +3,11 @@
 # Define your web browsers
 BROWSER="brave-browser"
 
+# change working directory to script's directory
+cd "$(dirname "$0")"
+
 # Generate a custom list of choices (Google, YouTube, and Torn)
-options="Apps\nGo\nYou\nTorn\nChat"
+options="Apps\nGo\nYou\nTorn\nChat\nGitHub\nWassap\nKeep\nMusic\nLogout\nShutdown\nReboot"
 
 # Show the list in Rofi and let the user select an option
 selected_option=$(echo -e "$options" | rofi -dmenu -mesg ">>> Tab = Autocomplete" -i -p "run: ")
@@ -28,13 +31,40 @@ case "$selected_option" in
     Chat)
         # Open the website www.torn.com
         xdg-open "https://chat.openai.com/"
-        ;;
+        ;;	
     Apps)
         # run apps
         rofi -combi-modi window,drun -show combi
         ;;
+    GitHub)
+        # Open the website www.torn.com
+        xdg-open "https://github.com/mohamadalzhori?tab=repositories"
+        ;;	
+    Logout)
+	# Logout using Qtile's exit command
+        qtile cmd-obj -o cmd -f shutdown
+	;;
+    Shutdown)
+        # Shutdown the system
+        systemctl poweroff
+        ;;
+    Reboot)
+        # Reboot the system
+        systemctl reboot
+        ;;
+    Music)
+        rhythmbox
+        ;;	
+    Wassap)
+        # Open whatsapp.com
+	xdg-open "https://web.whatsapp.com"
+        ;;
+    Keep)
+    	xdg-open "https://keep.google.com"	    
+	;;
     *)
         # If no valid option selected, exit
         exit
         ;;
 esac
+
