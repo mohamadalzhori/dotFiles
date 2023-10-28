ce3336d0de5d1c89f2ca42b924af3b47d598bc03
Mohamad Alzhori <99711702+mohamadalzhori@users.noreply.github.com>
2023-10-28 11:44:51 +0300

    added fusuma and other apps

--- autostart.sh
+++ autostart.sh
@@ -8,12 +8,19 @@ if ! pgrep -x "redshift" > /dev/null; then
 fi
 
 
-feh --bg-scale ~/Pictures/Wallpapers/pop.jpg &
+#feh --bg-scale ~/Pictures/Wallpapers/AlQantara.png &
+feh --bg-center Pictures/Wallpapers/TheWallpaper.png&
 nm-applet &
 #dunst &
 #sxhkd &
+#/bin/bash /home/zhori/.local/bin/scripts/onedrive.sh &
+#megasync &
 blueman-applet &
 mailspring &
 copyq &
 picom &
 todoist &
+mocp -m Music &
+fusuma &
+#notion-app-enhanced &
+
